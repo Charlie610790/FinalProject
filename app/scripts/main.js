827f24706b4c34ac88408015e65e2b6ac24e329d
Charlie Jones <myupstateproperties@gmail.com>
2014-06-24 21:29:58 -0400

    load and install Headroom.JS into all pages of project and setup JS and JQuery to work with Headroom.  Resize header toolbars to work with fixed position userbar.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('\'Allo \'Allo!');
+
+// grab an element
+var myElement = document.querySelector(".userBarIndex");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init(); 
+
+$(".userBarIndex").headroom();
