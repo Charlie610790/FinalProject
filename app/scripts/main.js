f53517de78d4772a65e0f34fb7b17bbd2113e34f
Charlie Jones <myupstateproperties@gmail.com>
2014-06-27 10:35:21 -0400

    adding in more pages as well as ninja slider js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-console.log('\'Allo \'Allo!');
+//////////////HEADROOM.JS////BEGIN
 
 // grab an element
 var myElement = document.querySelector(".userBarIndex");
@@ -8,3 +8,34 @@ var headroom  = new Headroom(myElement);
 headroom.init(); 
 
 $(".userBarIndex").headroom();
+
+//////////////HEADROOM.JS////END
+
+
+
+//////////////GOOGLEMAPS////BEGIN
+function initialize() {
+  var mapOptions = {
+    zoom: 17,
+    center: new google.maps.LatLng(42.346354, -71.098059)
+  };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+      'callback=initialize';
+  document.body.appendChild(script);
+}
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+// window.onload = loadScript;
+
+//////////////GOOGLEMAPS////END
