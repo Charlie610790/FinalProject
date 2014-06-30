1cdf5aa12465a2f9fee0482cac269cb93f34b253
Charlie Jones <myupstateproperties@gmail.com>
2014-06-30 15:03:57 -0400

    add footer to all pages and add/setup team info players page

--- app/scripts/googleMaps.js
+++ app/scripts/googleMaps.js
@@ -1,23 +1,25 @@
+'use strict';
+
 //////////////GOOGLEMAPS////BEGIN
 function initialize() {
-  var mapOptions = {
-    zoom: 17,
-    center: new google.maps.LatLng(42.346354, -71.098059)
-  };
+    var mapOptions = {
+        zoom: 17,
+        center: new google.maps.LatLng(42.346354, -71.098059)
+    };
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
 
 function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
       'callback=initialize';
-  document.body.appendChild(script);
+    document.body.appendChild(script);
 }
 
-  google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,15 @@
+'use strict';
+
 //////////////HEADROOM.JS////BEGIN
 
 // grab an element
-var myElement = document.querySelector(".userBarIndex");
+var myElement = document.querySelector('.userBarIndex');
 // construct an instance of Headroom, passing the element
 var headroom  = new Headroom(myElement);
 // initialise
-headroom.init(); 
+headroom.init();
 
-$(".userBarIndex").headroom();
+$('.userBarIndex').headroom();
 
 //////////////HEADROOM.JS////END
 
