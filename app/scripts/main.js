438733858a6479711b9889cccc82cabb7906f237
Charlie Jones <myupstateproperties@gmail.com>
2014-07-07 09:14:22 -0400

    successfully adding users

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,44 +25,29 @@ var UserCollection = Parse.Collection.extend({
 
 var collection = new UserCollection();
 
-// $('.signupButton').click(function() {
-//     //takse the input value of the comment field and assigns it to the var comment
-//     var comment = $('#imageComment').val();
-//     //takse the input value of the url field and assigns it to the var url
-//     var url = $('#imageURL').val();
-//     //creates a new instance of the constructor POST passing the two assigned variables
-//     var outgoingPost = new Post();
-//     //sets the two assigned variables
-//     outgoingPost.set({
-//         'URL'     :   url,
-//         'Comment' :   comment
-//     });
+$('.signupButton').click(function() {
+	var user = new Parse.User();
 
-//     outgoingPost.save().done(function () {
-//         console.log('It Worked!');
-//         console.log('url is', url);
-//         console.log('comment is', comment);
-//         //takes the value of the comment and image fields and clears it
-//         $('#imageComment').val('');
-//         $('#imageURL').val('');
-//     });
+	var username = 	$('#userName').val();
+	var password = 	$('#password').val();
+	var email 	 = 	$('#email').val();
 
-// });
+	user.set({
+	'username' :  username,
+	'password' :  password,
+	'email'    :  email
+	});
 
-var user = new Parse.User();
-user.set("username", "my name");
-user.set("password", "my pass");
-user.set("email", "email@example.com");
- 
-// other fields can be set just like with Parse.Object
-user.set("phone", "415-392-0202");
- 
-user.signUp(null, {
-  success: function(user) {
+	user.signUp(null, {
+  	success: function(user) {
     // Hooray! Let them use the app now.
-  },
-  error: function(user, error) {
+  	},
+  	error: function(user, error) {
     // Show the error message somewhere and let the user try again.
     alert("Error: " + error.code + " " + error.message);
-  }
+  	}
+	});
+
+	console.log('It Worked!');
+
 });
