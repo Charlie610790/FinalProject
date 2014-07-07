c3ba5a32e770424c3f20e2c2570543cd06ad94c2
Charlie Jones <myupstateproperties@gmail.com>
2014-07-07 08:42:46 -0400

    starting with parse

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+Parse.initialize('Y0CLQUqpAgyNIY0aA2qx1nW6GWyqM1cUcdKGo2BR', 'RZCmPbmSapjeCMfWQNUqmiTfInXos8vxS8xkUBPa');
+
 //////////////HEADROOM.JS////BEGIN
 
 // grab an element
@@ -12,3 +14,21 @@ headroom.init();
 $('.userBarIndex').headroom();
 
 //////////////HEADROOM.JS////END
+
+var user = new Parse.User();
+user.set("username", "my name");
+user.set("password", "my pass");
+user.set("email", "email@example.com");
+ 
+// other fields can be set just like with Parse.Object
+user.set("phone", "415-392-0202");
+ 
+user.signUp(null, {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
