fc9332458b6bc7d345b4a681783a2d85dbc48b29
Charlie Jones <myupstateproperties@gmail.com>
2014-07-09 10:51:48 -0400

    starting with geo

---
+++ app/scripts/geo.js
@@ -0,0 +1,33 @@
+// function checkGeoSuport() {
+// 	if (Modernizr.geolocation) {
+// 		console.log('This device supports geolocation.');
+// 	} else {
+// 		// no native support; maybe try a fallback?
+// 		alert('This device does not support geolocation.');
+// 		console.log('This device does not support geolocation.');
+// 	}
+// }
+
+// checkGeoSuport();
+
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log('Latitude : ' + crd.latitude);
+  console.log('Longitude: ' + crd.longitude);
+  console.log('More or less ' + crd.accuracy + ' meters.');
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
