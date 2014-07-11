ab24f1c80c03d6620431beea22aab33f79447311
Charlie Jones <myupstateproperties@gmail.com>
2014-07-11 09:49:03 -0400

    setting user geolocation to parse user

--- app/scripts/geo.js
+++ app/scripts/geo.js
@@ -24,6 +24,16 @@ function success(pos) {
   console.log('Latitude : ' + crd.latitude);
   console.log('Longitude: ' + crd.longitude);
   console.log('More or less ' + crd.accuracy + ' meters.');
+
+  	var user = Parse.User.current();
+	var point = new Parse.GeoPoint(crd.latitude, crd.longitude);
+  	user.set("GeoLocation", point);
+  	user.save();
+
+  console.log(user);
+
+
+
 };
 
 function error(err) {
@@ -31,3 +41,30 @@ function error(err) {
 };
 
 navigator.geolocation.getCurrentPosition(success, error, options);
+
+////////////
+
+
+	// var user = Parse.User.current();
+	// var point = new Parse.GeoPoint(crd.latitude, crd.longitude);
+ //  	user.set("GeoLocation", point);
+ //  	user.save();
+
+$('.nearMe').click(function() {
+	var user = Parse.User.current();
+	var Stadium = Parse.Object.extend("Stadium");
+	var userGeoPoint = user.get("location");
+	var query = new Parse.Query(Stadium);
+	query.near("location", userGeoPoint);
+// Limit what could be a lot of points.
+	query.limit(3);
+// Final list of objects
+	query.find({
+  	success: function(Stadiums) {
+  		return
+  	}
+});
+	console.log('You Ran It');
+
+});
+
