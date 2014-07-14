a5088caab5137df157cc2074ff57312be1c8c660
Charlie Jones <myupstateproperties@gmail.com>
2014-07-14 11:45:49 -0400

    adding new beestings in for beenthere template

--- app/scripts/mystadiumsBTView.js
+++ app/scripts/mystadiumsBTView.js
@@ -28,27 +28,14 @@ var MyStadiumsBTView = Parse.View.extend({
     },
  
 });
-//Need to figure out how to point to current users related BT stadiums
         var user = Parse.User.current();
         var relation = user.relation("BTstadiums");
-        // var query = new Parse.Query(user);
-        // query.equalTo("BTstadiums", "StadiumName");
-        // console.log(this);
         relation.query().find({
 
             success: function(stadiums) {
                 console.log(stadiums)
                 stadiums.forEach(function(stadium){
-                    new MyStadiumsBTView({model: stadium})
-                })
-                // for (var i = 0; i < results.length; i++) { 
-                //     var object = results[i];
-
-                //     console.log(object.get('StadiumName'));
-                //     console.log(object.get('TeamLogo'));
-                // }
-                // this.render();
-
+                    new MyStadiumsBTView({model: stadium.attributes})
+                });
             }
-        })
-// new MyStadiumsBTView(Parse.User.current().attributes.B);
+        });
