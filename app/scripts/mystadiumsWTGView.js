0cccca7b8c854c740a53ce136dab6f20e360162b
Charlie Jones <myupstateproperties@gmail.com>
2014-07-14 12:04:27 -0400

    adding been there and want to go pages

---
+++ app/scripts/mystadiumsWTGView.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var MyStadiumsWTGView = Parse.View.extend({
+ 
+    className: 'Stadium',
+
+    collection: 'StadiumsCollection',
+
+ 
+    myStadiumsWTGTemplate: _.template($('.myStadiumsWTGTemplate').text()),
+
+    events: {
+        
+    },
+ 
+ 
+    initialize: function(){
+        console.log('hi')
+        // console.log(this.model)
+        $('.myStadiumsWTGContainer').append(this.el);
+        this.render();
+
+    },
+ 
+    render: function(){
+        var renderedTemplate = this.myStadiumsWTGTemplate(this.model);
+        this.$el.html(renderedTemplate);
+    },
+ 
+});
+        var user = Parse.User.current();
+        var relation = user.relation("WTGstadiums");
+        relation.query().find({
+
+            success: function(stadiums) {
+                console.log(stadiums)
+                stadiums.forEach(function(stadium){
+                    new MyStadiumsWTGView({model: stadium.attributes})
+                });
+            }
+        });
