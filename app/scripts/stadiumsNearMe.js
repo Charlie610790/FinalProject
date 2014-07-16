0c350d56579f886ac822cb4b2f53e7d6003b7a33
Charlie Jones <myupstateproperties@gmail.com>
2014-07-15 21:09:10 -0400

    removing imcomplete parts of the application

--- app/scripts/stadiumsNearMe.js
+++ app/scripts/stadiumsNearMe.js
@@ -10,8 +10,6 @@ var StadiumNearMeView = Parse.View.extend({
     stadiumsnearmeTemplate: _.template($('.stadiumsnearmeTemplate').text()),
 
     events: {
-        'click .mlbStadiumsNearMe'              : 'mlbResults',
-        'click .AStadiumsNearMe'              : 'AResults',
         
     },
  
@@ -25,9 +23,10 @@ var StadiumNearMeView = Parse.View.extend({
         var renderedTemplate = this.stadiumsnearmeTemplate(this.model.attributes);
         this.$el.html(renderedTemplate);
         
-    },
+    },    
+ 
+});
 
-    mlbResults: function(){
     var user = Parse.User.current();
     var Stadium = Parse.Object.extend("Stadium");
     var userGeoPoint = user.get("GeoLocation");
@@ -37,55 +36,9 @@ var StadiumNearMeView = Parse.View.extend({
     query.limit(3);
     // Final list of objects
     query.find({
-    success: function(results) {
-        for (var i = 0; i < results.length; i++) { 
-        var object = results[i];
-
-        console.log(object.get('StadiumName'));
-        console.log(object.get('TeamLogo'));
-
-
-        // $('.stadiumsList').append(this.el);
-        // this.render();
-
-        }
-        alert("Successfully retrieved " + results.length + " stadiums.");
-
-    }
-});
-    console.log('You Ran It');
-        
-    },
-
-    AResults: function(){
-    var user = Parse.User.current();
-    var Stadium = Parse.Object.extend("ABaseballStadiums");
-    var userGeoPoint = user.get("GeoLocation");
-    var query = new Parse.Query(Stadium);
-    query.near("GeoLocation", userGeoPoint);
-    // Limit what could be a lot of points.
-    query.limit(3);
-    // Final list of objects
-    query.find({
-    success: function(results) {
-        for (var i = 0; i < results.length; i++) { 
-        var object = results[i];
-
-        
-
-        console.log(object.get('StadiumName'));
-        console.log(object.get('TeamLogo'));
-
-
-        }
-        alert("Successfully retrieved " + results.length + " stadiums.");
-
-    }
+        success: function(stadiums) {
+                stadiums.forEach(function(stadium){
+                    new StadiumNearMeView({model: stadium.attributes})
+                });
+            }
     });
-    console.log('You Ran It');
-        
-    },
- 
-});
-
-new StadiumNearMeView({model: Parse.User.current()});
